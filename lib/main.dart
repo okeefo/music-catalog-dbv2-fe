ae193a03e08356b48f3205c680cdc1631a704645
Keith Pyle <keith.pyle@gmail.com>
2024-09-09 00:42:41 +0100

    Initial setup for Flutter frontend

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Catalog',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Catalog'),
+      ),
+      body: Center(
+        child: Text('Welcome to Music Catalog'),
+      ),
+    );
+  }
+}
