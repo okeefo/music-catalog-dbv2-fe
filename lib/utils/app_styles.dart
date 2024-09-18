114fe7b0105d326b8bb5c426f0210577ee2a5e96
Keith Pyle <keith.pyle@gmail.com>
2024-09-19 00:16:13 +0100

    Light Dark Theme changes

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -5,4 +5,8 @@ class AppTheme {
   static const Color light = Color(0xffffffff);
   static const Color darkText = Color(0xffffa500); // Orange color for dark mode text
   static const Color lightText = Color(0xff000000); // Black color for light mode text
+  static const Color darkSideBarText = Color.fromARGB(255, 219, 218, 218); // Black color for dark mode sidebar text
+  static const Color darkSideBarIconColor = Color.fromARGB(255, 219, 218, 218); // Black color for dark mode sidebar icon
+  static const Color lightSideBarText = Color(0xff1e1e1e); // Black color for light mode sidebar text
+  static const Color lightSideBarIconColor = Color(0xff1e1e1e); // Black color for light mode sidebar icon
 }
