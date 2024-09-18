44e6908da50175e6895d88fd19ad0397a6fbb0f2
Keith Pyle <keith.pyle@gmail.com>
2024-09-19 00:35:09 +0100

    Theme updated

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static const Color dark = Color(0xff1e1e1e);
   static const Color light = Color(0xffffffff);
+  static const Color grey = Color.fromARGB(255, 76, 75, 75); // Black color for light mode sidebar icon
   static const Color darkText = Color(0xffffa500); // Orange color for dark mode text
   static const Color lightText = Color(0xff000000); // Black color for light mode text
   static const Color darkSideBarText = Color.fromARGB(255, 219, 218, 218); // Black color for dark mode sidebar text
