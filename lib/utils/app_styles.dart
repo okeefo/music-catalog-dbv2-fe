c9cdf98b084919b43c21118ca7c5848980b95109
Keith Pyle <keith.pyle@gmail.com>
2024-09-18 22:44:04 +0100

    sidebar added

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color dark = Color(0xff1e1e1e);
+  static const Color light = Color(0xffffffff);
+  static const Color darkText = Color(0xffffa500); // Orange color for dark mode text
+  static const Color lightText = Color(0xff000000); // Black color for light mode text
+}
