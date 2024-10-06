dc2c33f0b69d4b942e70697182a3aa334e2f1c94
Keith Pyle <keith.pyle@gmail.com>
2024-10-06 18:40:26 +0100

    bug fixing and refactoring theme changes

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:front_end/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../utils/app_styles.dart';
 
 class ThemeProvider with ChangeNotifier {
-  Color _darkFontColor = AppTheme.defaultDarkFontColor;
-  Color _lightFontColor = AppTheme.defaultLightFontColor;
-  Color _darkBoldFontColor = AppTheme.defaultDarkBoldFontColor;
-  Color _lightBoldFontColor = AppTheme.defaultLightBoldFontColor;
-  Color _darkIconColor = AppTheme.defaultDarkIconColor;
-  Color _lightIconColor = AppTheme.defaultLightIconColor;
+  Color _darkFontColour = AppTheme.defaultDarkFontColor;
+  Color _lightFontColour = AppTheme.defaultLightFontColor;
+  Color _darkBoldFontColour = AppTheme.defaultDarkBoldFontColor;
+  Color _lightBoldFontColour = AppTheme.defaultLightBoldFontColor;
+  Color _darkIconColour = AppTheme.defaultDarkIconColor;
+  Color _lightIconColour = AppTheme.defaultLightIconColor;
   Brightness _brightness = AppTheme.brightness;
 
   ThemeProvider() {
@@ -17,17 +16,17 @@ class ThemeProvider with ChangeNotifier {
   }
 
   Brightness get brightness => _brightness;
-  Color get fontColor => _isDarkMode() ? _darkFontColor : _lightFontColor;
-  Color get boldFontColor => _isDarkMode() ? _darkBoldFontColor : _lightBoldFontColor;
-  Color get iconColor => _isDarkMode() ? _darkIconColor : _lightIconColor;
+  Color get fontColour => _isDarkMode() ? _darkFontColour : _lightFontColour;
+  Color get boldFontColour => _isDarkMode() ? _darkBoldFontColour : _lightBoldFontColour;
+  Color get iconColour => _isDarkMode() ? _darkIconColour : _lightIconColour;
   Color get backgroundColour => _isDarkMode() ? AppTheme.darkBackground : AppTheme.lightBackground;
   Color get greyBackground => AppTheme.greyBackground;
-  Color get darkFontColor => _darkFontColor;
-  Color get lightFontColor => _lightFontColor;
-  Color get darkBoldFontColor => _darkBoldFontColor;
-  Color get lightBoldFontColor => _lightBoldFontColor;
-  Color get darkIconColor => _darkIconColor;
-  Color get lightIconColor => _lightIconColor;
+  Color get darkFontColour => _darkFontColour;
+  Color get lightFontColour => _lightFontColour;
+  Color get darkBoldFontColour => _darkBoldFontColour;
+  Color get lightBoldFontColour => _lightBoldFontColour;
+  Color get darkIconColour => _darkIconColour;
+  Color get lightIconColour => _lightIconColour;
   bool get isDarkMode => _brightness == Brightness.dark;
 
   bool _isDarkMode() {
@@ -35,37 +34,37 @@ class ThemeProvider with ChangeNotifier {
   }
 
   void setDarkFontColor(Color color) {
-    _darkFontColor = color;
+    _darkFontColour = color;
     _savePreferences();
     notifyListeners();
   }
 
   void setLightFontColor(Color color) {
-    _lightFontColor = color;
+    _lightFontColour = color;
     _savePreferences();
     notifyListeners();
   }
 
   void setDarkBoldFontColor(Color color) {
-    _darkBoldFontColor = color;
+    _darkBoldFontColour = color;
     _savePreferences();
     notifyListeners();
   }
 
   void setLightBoldFontColor(Color color) {
-    _lightBoldFontColor = color;
+    _lightBoldFontColour = color;
     _savePreferences();
     notifyListeners();
   }
 
   void setDarkIconColor(Color color) {
-    _darkIconColor = color;
+    _darkIconColour = color;
     _savePreferences();
     notifyListeners();
   }
 
   void setLightIconColor(Color color) {
-    _lightIconColor = color;
+    _lightIconColour = color;
     _savePreferences();
     notifyListeners();
   }
@@ -78,24 +77,24 @@ class ThemeProvider with ChangeNotifier {
 
   Future<void> _loadPreferences() async {
     final prefs = await SharedPreferences.getInstance();
-    _darkFontColor = Color(prefs.getInt('darkFontColor') ?? AppTheme.defaultDarkFontColor.value);
-    _lightFontColor = Color(prefs.getInt('lightFontColor') ?? AppTheme.defaultLightFontColor.value);
-    _darkBoldFontColor = Color(prefs.getInt('darkBoldFontColor') ?? AppTheme.defaultDarkBoldFontColor.value);
-    _lightBoldFontColor = Color(prefs.getInt('lightBoldFontColor') ?? AppTheme.defaultLightBoldFontColor.value);
-    _darkIconColor = Color(prefs.getInt('darkIconColor') ?? AppTheme.defaultDarkIconColor.value);
-    _lightIconColor = Color(prefs.getInt('lightIconColor') ?? AppTheme.defaultLightIconColor.value);
+    _darkFontColour = Color(prefs.getInt('darkFontColor') ?? AppTheme.defaultDarkFontColor.value);
+    _lightFontColour = Color(prefs.getInt('lightFontColor') ?? AppTheme.defaultLightFontColor.value);
+    _darkBoldFontColour = Color(prefs.getInt('darkBoldFontColor') ?? AppTheme.defaultDarkBoldFontColor.value);
+    _lightBoldFontColour = Color(prefs.getInt('lightBoldFontColor') ?? AppTheme.defaultLightBoldFontColor.value);
+    _darkIconColour = Color(prefs.getInt('darkIconColor') ?? AppTheme.defaultDarkIconColor.value);
+    _lightIconColour = Color(prefs.getInt('lightIconColor') ?? AppTheme.defaultLightIconColor.value);
     _brightness = Brightness.values[prefs.getInt('brightness') ?? Brightness.light.index];
     notifyListeners();
   }
 
   Future<void> _savePreferences() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setInt('darkFontColor', _darkFontColor.value);
-    prefs.setInt('lightFontColor', _lightFontColor.value);
-    prefs.setInt('darkBoldFontColor', _darkBoldFontColor.value);
-    prefs.setInt('lightBoldFontColor', _lightBoldFontColor.value);
-    prefs.setInt('darkIconColor', _darkIconColor.value);
-    prefs.setInt('lightIconColor', _lightIconColor.value);
+    prefs.setInt('darkFontColor', _darkFontColour.value);
+    prefs.setInt('lightFontColor', _lightFontColour.value);
+    prefs.setInt('darkBoldFontColor', _darkBoldFontColour.value);
+    prefs.setInt('lightBoldFontColor', _lightBoldFontColour.value);
+    prefs.setInt('darkIconColor', _darkIconColour.value);
+    prefs.setInt('lightIconColor', _lightIconColour.value);
     prefs.setInt('brightness', _brightness.index);
   }
 }
