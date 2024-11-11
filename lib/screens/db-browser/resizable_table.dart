3684daa75768cc18a10d57c8149f1ab695738e8e
Keith Pyle <keith.pyle@gmail.com>
2024-11-11 01:54:44 +0000

    Resizable Table - New Features
    * added the discogs Release Id as a hyperlink,
    * added feature to hide a column.
    * added feature to order columns by an index - Can be expanded in the future to change the ordering of fields.

--- lib/screens/db-browser/resizable_table.dart
+++ lib/screens/db-browser/resizable_table.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart' as material;
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:front_end/screens/db-browser/track_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResizableTable extends StatefulWidget {
-  final List<String> headers;
+  final List<TrackColumn> headers;
   final List<List<String>> data;
   final material.TextStyle rowStyle;
   final material.TextStyle headerStyle;
@@ -26,6 +28,7 @@ class ResizableTableState extends State<ResizableTable> {
   void initState() {
     super.initState();
     columnWidths = List<double>.filled(widget.headers.length, 0.0);
+    columnWidths = _calculateColumnWidths();
   }
 
   @override
@@ -46,10 +49,14 @@ class ResizableTableState extends State<ResizableTable> {
     List<double> widths = List<double>.filled(widget.headers.length, 0.0);
 
     for (int i = 0; i < widget.headers.length; i++) {
+      if (!widget.headers[i].isVisible) {
+        widths[i] = 0.0;
+        continue;
+      }
       // Measure header width
-      textPainter.text = material.TextSpan(text: widget.headers[i], style: widget.headerStyle);
+      textPainter.text = material.TextSpan(text: widget.headers[i].name, style: widget.headerStyle);
       textPainter.layout();
-      widths[i] = textPainter.width + 40.0; // 8.0 padding on each side
+      widths[i] = textPainter.width + 20.0; // 8.0 padding on each side
 
       // Measure each cell in the column
       for (var row in widget.data) {
@@ -74,20 +81,22 @@ class ResizableTableState extends State<ResizableTable> {
           children: [
             Row(
               children: List.generate(widget.headers.length, (index) {
-                return _buildResizableColumn(index, widget.headers[index]);
+                if (!widget.headers[index].isVisible) {
+                  return Container(); // Skip rendering the header if not visible
+                }
+                return _buildResizableColumn(index, widget.headers[index].name);
               }),
             ),
             ...widget.data.map((row) {
               return Row(
                 children: List.generate(row.length, (index) {
-                  return Container(
-                    width: columnWidths[index],
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(row[index], style: widget.rowStyle),
-                  );
+                  if (!widget.headers[index].isVisible) {
+                    return Container(); // Skip rendering the cell if the column is not visible
+                  }
+                  return _buildCell(index, row[index], row);
                 }).toList(),
               );
-            }),
+            }).toList(),
           ],
         ),
       ),
@@ -107,10 +116,47 @@ class ResizableTableState extends State<ResizableTable> {
         child: Row(
           children: [
             Expanded(child: Text(header, style: widget.headerStyle)),
-            const Icon(material.Icons.drag_handle),
+            Container(
+              width: 2.0,
+              height: 10.0,
+              color: material.Colors.grey,
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCell(int index, String text, List<String> row) {
+    if (widget.headers[index] == TrackColumns.discogsId) {
+      final url = row[TrackColumns.discogsUrl.index]; // Get the URL from the corresponding column
+      return GestureDetector(
+        onDoubleTap: () async {
+          final uri = Uri.parse(url);
+          if (await canLaunchUrl(uri)) {
+            await launchUrl(uri);
+          }
+        },
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: Container(
+            width: columnWidths[index],
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              text,
+              style: widget.rowStyle.copyWith(
+                color: material.Colors.blue,
+                decoration: material.TextDecoration.underline,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    return Container(
+      width: columnWidths[index],
+      padding: const EdgeInsets.all(8.0),
+      child: Text(text, style: widget.rowStyle),
     );
   }
 }
