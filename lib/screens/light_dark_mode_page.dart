2525a245555f680fe60f6a71797730a903fd5da8
Keith Pyle <keith.pyle@gmail.com>
2024-11-24 18:33:24 +0000

    Extra controls over infinite scrolling - added a threshold of when to load

--- lib/screens/light_dark_mode_page.dart
+++ lib/screens/light_dark_mode_page.dart
@@ -17,90 +17,144 @@ class _LightDarkModePageState extends State<LightDarkModePage> {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return ScaffoldPage(
+      header: PageHeader(title: Text('Light/Dark Mode Settings')),
+      content: Column(
         children: [
           Expanded(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ToggleSwitch(
-                    checked: themeProvider.isDarkMode,
-                    onChanged: (isDarkMode) {
-                      themeProvider.setBrightness(isDarkMode ? Brightness.dark : Brightness.light);
-                    },
-                    content: Text(
-                      themeProvider.isDarkMode ? 'Dark Mode' : 'Light Mode',
-                      style: TextStyle(
-                        color: themeProvider.fontColour,
+            child: Row(
+              children: [
+                Flexible(flex: 1, child: Container()), // Empty space
+                Expanded(
+                  flex: 4,
+                  child: _buildGridBox(
+                    title: 'Basic',
+                    themeProvider: themeProvider,
+                    children: [
+                      ColorPickerRow(
+                        text: 'Icon Color',
+                        color: themeProvider.iconColour,
+                        onColorChanged: (color) {
+                          themeProvider.setIconColour(color);
+                        },
+                        isDarkMode: themeProvider.isDarkMode,
+                        darkFontColor: themeProvider.darkFontColour,
+                        lightFontColor: themeProvider.lightFontColour,
                       ),
+                      ColorPickerRow(
+                        text: 'Font Color',
+                        color: themeProvider.fontColour,
+                        onColorChanged: (color) {
+                          themeProvider.setFontColour(color);
+                        },
+                        isDarkMode: themeProvider.isDarkMode,
+                        darkFontColor: themeProvider.darkFontColour,
+                        lightFontColor: themeProvider.lightFontColour,
+                      ),
+                      ColorPickerRow(
+                        text: 'Bold Font Color',
+                        color: themeProvider.boldFontColour,
+                        onColorChanged: (color) {
+                          themeProvider.setBoldFontColor(color);
+                        },
+                        isDarkMode: themeProvider.isDarkMode,
+                        darkFontColor: themeProvider.darkFontColour,
+                        lightFontColor: themeProvider.lightFontColour,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: _buildGridBox(
+                    title: 'Table',
+                    themeProvider: themeProvider,
+                    children: [
+                      ColorPickerRow(
+                        text: 'Table Border Color',
+                        color: themeProvider.tableBorderColour,
+                        onColorChanged: (color) {
+                          themeProvider.setTableBorderColour(color);
+                        },
+                        isDarkMode: themeProvider.isDarkMode,
+                        darkFontColor: themeProvider.darkFontColour,
+                        lightFontColor: themeProvider.lightFontColour,
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(flex: 1, child: Container()), // Empty space
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0), // Add padding at the bottom
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ToggleSwitch(
+                  checked: themeProvider.isDarkMode,
+                  onChanged: (isDarkMode) {
+                    themeProvider.setBrightness(isDarkMode ? Brightness.dark : Brightness.light);
+                  },
+                  content: Text(
+                    themeProvider.isDarkMode ? 'Dark Mode' : 'Light Mode',
+                    style: TextStyle(
+                      color: themeProvider.fontColour,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  ColorPickerRow(
-                    text: 'Bold Font Color',
-                    color: themeProvider.boldFontColour,
-                    onColorChanged: (color) {
-                     themeProvider.setBoldFontColor(color);
-                    },
-                    isDarkMode: themeProvider.isDarkMode,
-                    darkFontColor: themeProvider.darkFontColour,
-                    lightFontColor: themeProvider.lightFontColour,
+                ),
+                const SizedBox(width: 20),
+                Button(
+                  onPressed: () {
+                    if (themeProvider.isDarkMode) {
+                      themeProvider.resetDarkColors();
+                    } else {
+                      themeProvider.resetLightColors();
+                    }
+                  },
+                  child: Text(
+                    'Reset to Default',
+                    style: TextStyle(color: themeProvider.fontColour),
                   ),
-                  const SizedBox(height: 20),
-                  ColorPickerRow(
-                    text: 'Icon Color',
-                    color: themeProvider.iconColour,
-                    onColorChanged: (color) {
-                      themeProvider.setIconColour(color);
-                    },
-                    isDarkMode: themeProvider.isDarkMode,
-                    darkFontColor: themeProvider.darkFontColour,
-                    lightFontColor: themeProvider.lightFontColour,
-                  ),
-                  const SizedBox(height: 20),
-                  ColorPickerRow(
-                    text: 'Font Color',
-                    color: themeProvider.fontColour,
-                    onColorChanged: (color) {
-                      themeProvider.setFontColour(color);
-                    },
-                    isDarkMode: themeProvider.isDarkMode,
-                    darkFontColor: themeProvider.darkFontColour,
-                    lightFontColor: themeProvider.lightFontColour,
-                  ),
-                  const SizedBox(height: 20),
-                  ColorPickerRow(
-                    text: 'Table Border Color',
-                    color: themeProvider.tableBorderColour,
-                    onColorChanged: (color) {
-                      themeProvider.setTableBorderColour(color);
-                    },
-                    isDarkMode: themeProvider.isDarkMode,
-                    darkFontColor: themeProvider.darkFontColour,
-                    lightFontColor: themeProvider.lightFontColour,
-                  ),
-                  const SizedBox(height: 20),
-                  Button(
-                    onPressed: () {
-                      if (themeProvider.isDarkMode) {
-                        themeProvider.resetDarkColors();
-                      } else {
-                        themeProvider.resetLightColors();
-                      }
-                    },
-                    child: Text(
-                      'Reset to Default',
-                      style: TextStyle(color: themeProvider.fontColour),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildGridBox({
+    required String title,
+    required ThemeProvider themeProvider,
+    required List<Widget> children,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: themeProvider.tableBorderColour, width: 2.0),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  title,
+                  style: FluentTheme.of(context).typography.subtitle,
+                ),
+              ),
+              const SizedBox(height: 10),
+              ...children,
+            ],
+          ),
+        ),
       ),
     );
   }
