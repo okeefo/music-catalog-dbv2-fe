f27e9fa61a8a2ccb49b0c668983f406740ee18b6
Keith Pyle <keith.pyle@gmail.com>
2024-12-14 21:17:39 +0000

    updated backend urls

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -1,37 +1,37 @@
 class Endpoints {
   static late final String _baseUri;
-  static const String _activeDatabaseUri = 'databases/active';
-  static const String _configUri = 'config';
-  static const String _initialiseUri = 'databases';
-  static const String _mediaScanUri = 'media/scan';
-  static const String _tracks = 'tracks';
-  static const String _tracksCount = 'tracks/count';
+  static const String _activeDatabaseUri = 'v1/databases/active';
+  static const String _configUri = 'v1/config';
+  static const String _initialiseUri = 'v1/databases';
+  static const String _mediaScanUri = 'v1/media/scan';
+  static const String _tracks = 'v1/tracks';
+  static const String _tracksCount = 'v1/tracks/count';
 
   static void initialize(String baseUri) {
     _baseUri = baseUri;
   }
 
   static String getActiveDatabaseUri() {
-    return '$_baseUri/$_activeDatabaseUri';
+    return '$_baseUri$_activeDatabaseUri';
   }
 
   static String getConfigUri() {
-    return '$_baseUri/$_configUri';
+    return '$_baseUri$_configUri';
   }
 
   static String postCreateDataBaseUri() {
-    return '$_baseUri/$_initialiseUri';
+    return '$_baseUri$_initialiseUri';
   }
 
   static String scanForMusicUri() {
-    return '$_baseUri/$_mediaScanUri';
+    return '$_baseUri$_mediaScanUri';
   }
 
   static String getTracksUri() {
-    return '$_baseUri/$_tracks';
+    return '$_baseUri$_tracks';
   }
   
   static String getTotalTracksUri() {
-    return '$_baseUri/$_tracksCount';
+    return '$_baseUri$_tracksCount';
   }
 }
