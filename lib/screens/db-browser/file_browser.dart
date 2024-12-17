c3905926603af50bbb3f9a21264536933a4204f4
Keith Pyle <keith.pyle@gmail.com>
2024-12-17 00:17:58 +0000

    Added listeners to the File Browser -
    * this is prep for filtering.

--- lib/screens/db-browser/file_browser.dart
+++ lib/screens/db-browser/file_browser.dart
@@ -1,14 +1,15 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:front_end/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:logging/logging.dart';
 
 class FileBrowser extends StatefulWidget {
-  final Map<String, List<String>> publisherAlbums;
+  final Map<String, Set<String>> publisherAlbums;
 
   const FileBrowser({
-    super.key,
+    Key? key,
     required this.publisherAlbums,
-  });
+  }) : super(key: key);
 
   @override
   _FileBrowserState createState() => _FileBrowserState();
@@ -16,6 +17,7 @@ class FileBrowser extends StatefulWidget {
 
 class _FileBrowserState extends State<FileBrowser> {
   final Map<String, bool> _expandedPublishers = {};
+  final Logger _logger = Logger('FileBrowser');
 
   @override
   void initState() {
@@ -33,23 +35,32 @@ class _FileBrowserState extends State<FileBrowser> {
       itemCount: widget.publisherAlbums.keys.length,
       itemBuilder: (context, index) {
         String publisher = widget.publisherAlbums.keys.elementAt(index);
-        List<String> albums = widget.publisherAlbums[publisher]!;
+        Set<String> albums = widget.publisherAlbums[publisher]!;
         bool isExpanded = _expandedPublishers[publisher] ?? false;
 
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            GestureDetector(
-              onTap: () {
+            Listener(
+              onPointerDown: (event) => {
                 setState(() {
+                  _logger.info('Toggling expansion for $publisher');
                   _expandedPublishers[publisher] = !isExpanded;
-                });
+                }),
               },
               child: ListTile(
-                leading: Icon(FluentIcons.folder_list, color: themeProvider.iconColour),
-                title: Text(publisher,
-                    style: TextStyle(
-                        color: themeProvider.fontColour, fontSize: themeProvider.fontSizeDataTableRow, fontFamily: themeProvider.dataTableFontFamily)),
+                leading: Icon(
+                  isExpanded ? FluentIcons.folder_open : FluentIcons.folder_list,
+                  color: themeProvider.iconColour,
+                ),
+                title: Text(
+                  publisher,
+                  style: TextStyle(
+                    color: themeProvider.fontColour,
+                    fontSize: themeProvider.fontSizeDataTableRow,
+                    fontFamily: themeProvider.dataTableFontFamily,
+                  ),
+                ),
               ),
             ),
             if (isExpanded)
@@ -58,9 +69,24 @@ class _FileBrowserState extends State<FileBrowser> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: albums.map((album) {
-                    return ListTile(
-                      leading: const Icon(FluentIcons.folder),
-                      title: Text(album),
+                    return Listener(
+                      onPointerDown: (event) => {
+                        setState(() {
+                          _logger.info('Toggling expansion album $album for $publisher');
+                          _expandedPublishers[publisher] = !isExpanded;
+                        }),
+                      },
+                      child: ListTile(
+                        leading: const Icon(FluentIcons.music_note),
+                        title: Text(
+                          album,
+                          style: TextStyle(
+                            color: themeProvider.fontColour,
+                            fontSize: themeProvider.fontSizeDataTableRow,
+                            fontFamily: themeProvider.dataTableFontFamily,
+                          ),
+                        ),
+                      ),
                     );
                   }).toList(),
                 ),
