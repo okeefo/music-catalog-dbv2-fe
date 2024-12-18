9604c6a0af70abd35d38460b7e59677b78e246cc
Keith Pyle <keith.pyle@gmail.com>
2024-12-18 00:01:34 +0000

    Updated file browser selectors - preparing filters

--- lib/screens/db-browser/file_browser.dart
+++ lib/screens/db-browser/file_browser.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:front_end/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +19,10 @@ class FileBrowser extends StatefulWidget {
 
 class _FileBrowserState extends State<FileBrowser> {
   final Map<String, bool> _expandedPublishers = {};
+  final Set<String> _selectedPublishers = {};
+  final Set<String> _selectedAlbums = {};
   final Logger _logger = Logger('FileBrowser');
+  bool _isShiftPressed = false;
 
   @override
   void initState() {
@@ -27,73 +32,138 @@ class _FileBrowserState extends State<FileBrowser> {
     });
   }
 
+  bool anyAlbumSelected(Set<String> albums) {
+    return albums.any((album) => _selectedAlbums.contains(album));
+  }
+
+  bool shouldToggleExpansion(String publisher, Set<String> albums) {
+    return !isPublisherExpanded(publisher) || !anyAlbumSelected(albums);
+  }
+
+  void togglePublisherExpansion(String publisher) {
+    setState(() {
+      _expandedPublishers[publisher] = !isPublisherExpanded(publisher);
+    });
+  }
+
+  void removeSelectedAlbums(Set<String> albums) {
+    setState(() {
+      _selectedAlbums.removeAll(albums);
+    });
+  }
+
+  bool isPublisherExpanded(String publisher) {
+    return _expandedPublishers[publisher] ?? false;
+  }
+
+  void handlePublisherClick(String publisher, Set<String> albums) {
+    if (shouldToggleExpansion(publisher, albums)) {
+      togglePublisherExpansion(publisher);
+    }
+    removeSelectedAlbums(albums);
+  }
+
+  void handleShiftClick(String publisher, Set<String> albums) {
+    setState(() {
+      if (_selectedPublishers.contains(publisher)) {
+        _selectedPublishers.remove(publisher);
+      } else {
+        _selectedPublishers.add(publisher);
+        _selectedAlbums.removeAll(albums);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return ListView.builder(
-      itemCount: widget.publisherAlbums.keys.length,
-      itemBuilder: (context, index) {
-        String publisher = widget.publisherAlbums.keys.elementAt(index);
-        Set<String> albums = widget.publisherAlbums[publisher]!;
-        bool isExpanded = _expandedPublishers[publisher] ?? false;
+    return Focus(
+      autofocus: true,
+      onKey: (FocusNode node, RawKeyEvent event) {
+        setState(() {
+          _isShiftPressed = event.isShiftPressed;
+        });
+        return KeyEventResult.ignored;
+      },
+      child: ListView.builder(
+        itemCount: widget.publisherAlbums.keys.length,
+        itemBuilder: (context, index) {
+          String publisher = widget.publisherAlbums.keys.elementAt(index);
+          Set<String> albums = widget.publisherAlbums[publisher]!;
+          bool isExpanded = _expandedPublishers[publisher] ?? false;
+          bool isPublisherSelected = _selectedPublishers.contains(publisher);
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Listener(
-              onPointerDown: (event) => {
-                setState(() {
-                  _logger.info('Toggling expansion for $publisher');
-                  _expandedPublishers[publisher] = !isExpanded;
-                }),
-              },
-              child: ListTile(
-                leading: Icon(
-                  isExpanded ? FluentIcons.folder_open : FluentIcons.folder_list,
-                  color: themeProvider.iconColour,
-                ),
-                title: Text(
-                  publisher,
-                  style: TextStyle(
-                    color: themeProvider.fontColour,
-                    fontSize: themeProvider.fontSizeDataTableRow,
-                    fontFamily: themeProvider.dataTableFontFamily,
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Listener(
+                onPointerDown: (event) {
+                  if (_isShiftPressed) {
+                    handleShiftClick(publisher, albums);
+                  } else {
+                    handlePublisherClick(publisher, albums);
+                  }
+                },
+                child: Container(
+                  color: isPublisherSelected ? themeProvider.selectedRowColor : Colors.transparent,
+                  child: ListTile(
+                    leading: Icon(
+                      isExpanded ? FluentIcons.folder_open : FluentIcons.folder_list,
+                      color: themeProvider.iconColour,
+                    ),
+                    title: Text(
+                      publisher,
+                      style: TextStyle(
+                        color: themeProvider.fontColour,
+                        fontSize: themeProvider.fontSizeDataTableRow,
+                        fontFamily: themeProvider.dataTableFontFamily,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            if (isExpanded)
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: albums.map((album) {
-                    return Listener(
-                      onPointerDown: (event) => {
-                        setState(() {
-                          _logger.info('Toggling expansion album $album for $publisher');
-                          _expandedPublishers[publisher] = !isExpanded;
-                        }),
-                      },
-                      child: ListTile(
-                        leading: const Icon(FluentIcons.music_note),
-                        title: Text(
-                          album,
-                          style: TextStyle(
-                            color: themeProvider.fontColour,
-                            fontSize: themeProvider.fontSizeDataTableRow,
-                            fontFamily: themeProvider.dataTableFontFamily,
+              if (isExpanded)
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: albums.map((album) {
+                      bool isAlbumSelected = _selectedAlbums.contains(album);
+                      return Listener(
+                        onPointerDown: (event) => {
+                          setState(() {
+                            _logger.info('Toggling expansion album $album for $publisher');
+                            if (isAlbumSelected) {
+                              _selectedAlbums.remove(album);
+                            } else {
+                              _selectedAlbums.add(album);
+                            }
+                            _selectedPublishers.clear();
+                          }),
+                        },
+                        child: Container(
+                          color: isAlbumSelected ? themeProvider.selectedRowColor : Colors.transparent,
+                          child: ListTile(
+                            leading: Icon(FluentIcons.music_note, color: themeProvider.iconColour),
+                            title: Text(
+                              album,
+                              style: TextStyle(
+                                color: themeProvider.fontColour,
+                                fontSize: themeProvider.fontSizeDataTableRow,
+                                fontFamily: themeProvider.dataTableFontFamily,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }).toList(),
+                      );
+                    }).toList(),
+                  ),
                 ),
-              ),
-          ],
-        );
-      },
+            ],
+          );
+        },
+      ),
     );
   }
 }
