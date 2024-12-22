5d1d6a013b07441f9b6d62ad0c32363f333ef19c
Keith Pyle <keith.pyle@gmail.com>
2024-12-22 23:33:14 +0000

    Refactor

--- lib/screens/db-browser/publisher_browser.dart
+++ lib/screens/db-browser/publisher_browser.dart
@@ -1,18 +1,23 @@
-import 'dart:math';
-
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/services.dart';
 import 'package:front_end/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:logging/logging.dart';
 
+// Constants for repeated values
+const double rowHeight = 22.0;
+const double iconSize = 14.0;
+const EdgeInsets rowPadding = EdgeInsets.symmetric(vertical: 0.0);
+const EdgeInsets listTilePadding = EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0);
+const EdgeInsets albumListTilePadding = EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0);
+
 class PublisherBrowser extends StatefulWidget {
   final Map<String, Set<String>> publisherAlbums;
 
   const PublisherBrowser({
-    Key? key,
+    super.key,
     required this.publisherAlbums,
-  }) : super(key: key);
+  });
 
   @override
   _PublisherBrowserState createState() => _PublisherBrowserState();
@@ -32,23 +37,9 @@ class _PublisherBrowserState extends State<PublisherBrowser> {
     });
   }
 
-  bool anyAlbumSelected(Set<String> albums) {
-    return albums.any((album) => _selectedAlbums.contains(album));
-  }
-
-  bool shouldToggleExpansion(String publisher, Set<String> albums) {
-    return !isPublisherExpanded(publisher) || !anyAlbumSelected(albums);
-  }
-
   void togglePublisherExpansion(String publisher) {
     setState(() {
       _expandedPublishers[publisher] = !isPublisherExpanded(publisher);
-    });
-  }
-
-  void removeSelectedAlbums(Set<String> albums) {
-    setState(() {
-      _selectedAlbums.removeAll(albums);
     });
   }
 
@@ -82,7 +73,6 @@ class _PublisherBrowserState extends State<PublisherBrowser> {
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
 
     return ListView.builder(
       itemCount: widget.publisherAlbums.keys.length,
@@ -95,74 +85,119 @@ class _PublisherBrowserState extends State<PublisherBrowser> {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Listener(
-              onPointerDown: (event) {
-                handleSelectionClick(publisher, _selectedPublishers, _selectedAlbums);
-              },
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 0.0),
-                color: isPublisherSelected ? themeProvider.selectedRowColor : Colors.transparent,
-                child: SizedBox(
-                  height: 22.00,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
-                    leading: GestureDetector(
-                      onTap: () {
-                        handlePublisherIconClick(publisher);
-                      },
-                      child: Icon(
-                        isExpanded ? FluentIcons.folder_open : FluentIcons.folder_list,
-                        color: themeProvider.iconColour,
-                        size: 14,
-                      ),
-                    ),
-                    title: Text(
-                      publisher,
-                      style: TextStyle(
-                        color: themeProvider.fontColour,
-                        fontSize: themeProvider.fontSizeDataTableRow,
-                        fontFamily: themeProvider.dataTableFontFamily,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+            PublisherRow(
+              publisher: publisher,
+              isExpanded: isExpanded,
+              isSelected: isPublisherSelected,
+              onClick: () => handleSelectionClick(publisher, _selectedPublishers, _selectedAlbums),
+              onIconClick: () => handlePublisherIconClick(publisher),
             ),
             if (isExpanded)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: albums.map((album) {
-                  bool isAlbumSelected = _selectedAlbums.contains(album);
-                  return Listener(
-                    onPointerDown: (event) {
-                      handleSelectionClick(album, _selectedAlbums, _selectedPublishers);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 0.0),
-                      color: isAlbumSelected ? themeProvider.selectedRowColor : Colors.transparent,
-                      child: SizedBox(
-                        height: 22.00,
-                        child: ListTile(
-                          contentPadding: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
-                          leading: Icon(FluentIcons.music_note, color: themeProvider.iconColour, size: 14),
-                          title: Text(
-                            album,
-                            style: TextStyle(
-                              color: themeProvider.fontColour,
-                              fontSize: themeProvider.fontSizeDataTableRow,
-                              fontFamily: themeProvider.dataTableFontFamily,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
+              ...albums.map((album) {
+                bool isAlbumSelected = _selectedAlbums.contains(album);
+                return AlbumRow(
+                  album: album,
+                  isSelected: isAlbumSelected,
+                  onClick: () => handleSelectionClick(album, _selectedAlbums, _selectedPublishers),
+                );
+              }),
           ],
         );
       },
+    );
+  }
+}
+
+class PublisherRow extends StatelessWidget {
+  final String publisher;
+  final bool isExpanded;
+  final bool isSelected;
+  final VoidCallback onClick;
+  final VoidCallback onIconClick;
+
+  const PublisherRow({
+    super.key,
+    required this.publisher,
+    required this.isExpanded,
+    required this.isSelected,
+    required this.onClick,
+    required this.onIconClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Listener(
+      onPointerDown: (event) => onClick(),
+      child: Container(
+        padding: rowPadding,
+        color: isSelected ? themeProvider.selectedRowColor : Colors.transparent,
+        child: SizedBox(
+          height: rowHeight,
+          child: ListTile(
+            contentPadding: listTilePadding,
+            leading: GestureDetector(
+              onTap: onIconClick,
+              child: Icon(
+                isExpanded ? FluentIcons.folder_open : FluentIcons.folder_list,
+                color: themeProvider.iconColour,
+                size: iconSize,
+              ),
+            ),
+            title: Text(
+              publisher,
+              style: TextStyle(
+                color: themeProvider.fontColour,
+                fontSize: themeProvider.fontSizeDataTableRow,
+                fontFamily: themeProvider.dataTableFontFamily,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AlbumRow extends StatelessWidget {
+  final String album;
+  final bool isSelected;
+  final VoidCallback onClick;
+
+  const AlbumRow({
+    super.key,
+    required this.album,
+    required this.isSelected,
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Listener(
+      onPointerDown: (event) => onClick(),
+      child: Container(
+        padding: rowPadding,
+        color: isSelected ? themeProvider.selectedRowColor : Colors.transparent,
+        child: SizedBox(
+          height: rowHeight,
+          child: ListTile(
+            contentPadding: albumListTilePadding,
+            leading: Icon(FluentIcons.music_note, color: themeProvider.iconColour, size: iconSize),
+            title: Text(
+              album,
+              style: TextStyle(
+                color: themeProvider.fontColour,
+                fontSize: themeProvider.fontSizeDataTableRow,
+                fontFamily: themeProvider.dataTableFontFamily,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
